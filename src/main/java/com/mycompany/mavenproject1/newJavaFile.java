653f88fe2b24643778b262758343619c403752c0
stivenPRO500 <stidar02mal@gmail.com>
2023-02-10 14:15:42 -0600

    kk

---
+++ src/main/java/com/mycompany/mavenproject1/newJavaFile.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+import java.util.ArrayList;
+/**
+ *
+ * @author stida
+ */
+public class newJavaFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+         ArrayList palabras = new ArrayList();
+        palabras.add("god of war");
+        palabras.add("the last of us");
+        palabras.add("call of duty");
+        palabras.add("spiderman");
+        palabras.add("huncharted");
+        
+        int a = (int) (Math.random()*5);
+        
+        System.out.println("tu juego es:"+palabras.get(a));
+    }
+}
